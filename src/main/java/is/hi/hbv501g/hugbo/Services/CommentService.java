a5baaa3ef25c6e3f3adaf95c6f9537660b91afba
applemuffinhead <hkv5@hi.is>
2022-11-26 13:36:49 +0000

    Documentation for CommentService.java

--- src/main/java/is/hi/hbv501g/hugbo/Services/CommentService.java
+++ src/main/java/is/hi/hbv501g/hugbo/Services/CommentService.java
@@ -2,10 +2,19 @@ package is.hi.hbv501g.hugbo.Services;
 
 import is.hi.hbv501g.hugbo.Persistence.Entities.RecipeComments;
 
+/**
+ *
+ * This interface handles service for comments.
+ *
+ * @author Arnar Freyr
+ * @author Birgitta Yr
+ * @author Heba Solveig
+ * @author Hrefna Karen
+ *
+ */
 public interface CommentService {
     RecipeComments findByCommentID(long commentID);
     RecipeComments[] findByRecipeID(long recipeID);
     RecipeComments saveComment(RecipeComments newComment);
     void delete(RecipeComments recipeComments);
-
 }
