aaf6c3ed6c55570b5fc30c34e02f7c3e26d5a0ef
afs28 <70155488+afs28@users.noreply.github.com>
2022-11-26 13:37:22 +0000

    Documentation added to a couple of classes

--- src/main/java/is/hi/hbv501g/hugbo/Services/Implementation/RecipeServiceImplementation.java
+++ src/main/java/is/hi/hbv501g/hugbo/Services/Implementation/RecipeServiceImplementation.java
@@ -6,10 +6,18 @@ import is.hi.hbv501g.hugbo.Persistence.Repositories.RecipeUserRepository;
 import is.hi.hbv501g.hugbo.Services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+/**
+ * The implementation class for the RecipeService.
+ *
+ * @author Arnar Freyr
+ * @author Birgitta Yr
+ * @author Heba Solveig
+ * @author Hrefna Karen
+ *
+ */
 @Service
 public class RecipeServiceImplementation implements RecipeService {
     @Autowired
@@ -35,16 +43,35 @@ public class RecipeServiceImplementation implements RecipeService {
         return recipeRepository.findByRecipeID(recipeID);
     }
 
+    /**
+     * This function saves a recipe to the database.
+     * @param recipe to be saved.
+     * @return saved recipe.
+     */
     @Override
     public Recipe saveRecipe(Recipe recipe) {
         return recipeRepository.save(recipe);
     }
 
+    /**
+     * This function deletes a recipe from the database, was thought to be used at the beginning
+     * of the project but wasn't in the end. To be deleted!
+     * @param recipe to be deleted.
+     */
     @Override
     public void delete(Recipe recipe){
         recipeRepository.delete(recipe);
     }
 
+    /**
+     * This function allows the user to edit an existing recipe in the database.
+     * @param recipe to be edited.
+     * @param title to be edited.
+     * @param description to be edited.
+     * @param difficultyLevel to be edited.
+     * @param allergyFactors to be edited.
+     * @return edited recipe.
+     */
     @Override
     public Recipe editARecipe(Recipe recipe, String title, String description, String difficultyLevel, String allergyFactors) {
         recipe.setTitle(title);
--- src/main/java/is/hi/hbv501g/hugbo/Services/Implementation/RecipeUserServiceImplementation.java
+++ src/main/java/is/hi/hbv501g/hugbo/Services/Implementation/RecipeUserServiceImplementation.java
@@ -8,6 +8,15 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * The implementation class for the RecipeUserService.
+ *
+ * @author Arnar Freyr
+ * @author Birgitta Yr
+ * @author Heba Solveig
+ * @author Hrefna Karen
+ *
+ */
 @Service
 public class RecipeUserServiceImplementation implements RecipeUserService {
     private RecipeUserRepository recipeUserRepository;
@@ -17,20 +26,36 @@ public class RecipeUserServiceImplementation implements RecipeUserService {
         this.recipeUserRepository = recipeUserRepository;
     }
 
+    /**
+     * This function saves the user to the database.
+     * @param recipeUser to be saved.
+     * @return saved recipeUser.
+     */
     @Override
     public RecipeUser save(RecipeUser recipeUser){
         return recipeUserRepository.save(recipeUser);
     }
+
+    /**
+     * This function deletes the user from the database. Was thought to be used at first
+     * but turned out it wasn't, will be deleted.
+     * @param recipeUser to be deleted.
+     */
     @Override
     public void delete(RecipeUser recipeUser){
         recipeUserRepository.delete(recipeUser);
     }
 
-
+    /**
+     * This function enables the user to change the password to their account.
+     * @param recipeUser whose password will be changed.
+     * @param newPassword to the recipeUser account.
+     */
     @Override
     public void changePassword(RecipeUser recipeUser, String newPassword) {
         recipeUser.setRecipeUserPassword(newPassword);
     }
+
 
     @Override
     public List<RecipeUser> findAll(){
